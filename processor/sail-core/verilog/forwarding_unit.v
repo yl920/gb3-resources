1842f185b1a2bc68df4103506772ed34a3c188f3
Will Hewes <william.hewes03@gmail.com>
2025-05-29 15:04:05 +0100

    Optimise ForwardingUnit and imm_gen to reduce LUT usage and shorten EX-stage path

--- processor/sail-core/verilog/forwarding_unit.v
+++ processor/sail-core/verilog/forwarding_unit.v
@@ -40,36 +40,43 @@
  *	Forwarding Unit
  */
 
+`default_nettype none
 
+module ForwardingUnit (
+    input  wire [4:0]  rs1,
+    input  wire [4:0]  rs2,
+    input  wire [4:0]  MEM_RegWriteAddr,
+    input  wire [4:0]  WB_RegWriteAddr,
+    input  wire        MEM_RegWrite,
+    input  wire        WB_RegWrite,
+    input  wire [11:0] EX_CSRR_Addr,
+    input  wire [11:0] MEM_CSRR_Addr,
+    input  wire [11:0] WB_CSRR_Addr,
+    input  wire        MEM_CSRR,
+    input  wire        WB_CSRR,
+    output wire        MEM_fwd1,
+    output wire        MEM_fwd2,
+    output wire        WB_fwd1,
+    output wire        WB_fwd2
+);
 
-module ForwardingUnit(rs1, rs2, MEM_RegWriteAddr, WB_RegWriteAddr, MEM_RegWrite, WB_RegWrite, EX_CSRR_Addr, MEM_CSRR_Addr, WB_CSRR_Addr, MEM_CSRR, WB_CSRR, MEM_fwd1, MEM_fwd2, WB_fwd1, WB_fwd2);
-	input [4:0]	rs1;
-	input [4:0]	rs2;
-	input [4:0]	MEM_RegWriteAddr;
-	input [4:0]	WB_RegWriteAddr;
-	input		MEM_RegWrite;
-	input		WB_RegWrite;
-	input [11:0]	EX_CSRR_Addr;
-	input [11:0]	MEM_CSRR_Addr;
-	input [11:0]	WB_CSRR_Addr;
-	input		MEM_CSRR;
-	input		WB_CSRR;
-	output		MEM_fwd1;
-	output		MEM_fwd2;
-	output		WB_fwd1;
-	output		WB_fwd2;
+        wire rs1_eq_MEM = ~|(rs1 ^ MEM_RegWriteAddr);
+    wire rs2_eq_MEM = ~|(rs2 ^ MEM_RegWriteAddr);
 
-	/*
-	 *	if data hazard detected, assign RegWrite to decide if...
-	 *	result MEM or WB stage should be rerouted to ALU input
-	 */
-	assign MEM_fwd1 = (MEM_RegWriteAddr != 5'b0 && MEM_RegWriteAddr ==  rs1)?MEM_RegWrite:1'b0;
-	assign MEM_fwd2 = (MEM_RegWriteAddr != 5'b0 && MEM_RegWriteAddr ==  rs2 && MEM_RegWrite == 1'b1) || (EX_CSRR_Addr == MEM_CSRR_Addr && MEM_CSRR == 1'b1)?1'b1:1'b0;
+    wire rs1_eq_WB  = ~|(rs1 ^ WB_RegWriteAddr);
+    wire rs2_eq_WB  = ~|(rs2 ^ WB_RegWriteAddr);
 
-	/*
-	 *	from wb stage
-	 */
-	assign WB_fwd1 = (WB_RegWriteAddr != 5'b0 && WB_RegWriteAddr ==  rs1 && WB_RegWriteAddr != MEM_RegWriteAddr)?WB_RegWrite:1'b0;
-	assign WB_fwd2 = (WB_RegWriteAddr != 5'b0 && WB_RegWriteAddr ==  rs2 && WB_RegWrite == 1'b1 && WB_RegWriteAddr != MEM_RegWriteAddr) || (EX_CSRR_Addr == WB_CSRR_Addr && WB_CSRR == 1'b1 && MEM_CSRR_Addr != WB_CSRR_Addr)?1'b1:1'b0;
+    wire csr_EX_eq_MEM = ~|(EX_CSRR_Addr ^ MEM_CSRR_Addr);
+    wire csr_EX_eq_WB  = ~|(EX_CSRR_Addr ^ WB_CSRR_Addr);
+ 
+     assign MEM_fwd1 = MEM_RegWrite & rs1_eq_MEM & |MEM_RegWriteAddr;
+
+    assign MEM_fwd2 = (MEM_RegWrite & rs2_eq_MEM & |MEM_RegWriteAddr) |
+                      (MEM_CSRR     & csr_EX_eq_MEM);
+
+    assign WB_fwd1  = WB_RegWrite  & rs1_eq_WB  & |WB_RegWriteAddr & ~rs1_eq_MEM;
+
+    assign WB_fwd2  = (WB_RegWrite  & rs2_eq_WB  & |WB_RegWriteAddr & ~rs2_eq_MEM) |
+                      (WB_CSRR      & csr_EX_eq_WB & ~csr_EX_eq_MEM);
 
 endmodule
